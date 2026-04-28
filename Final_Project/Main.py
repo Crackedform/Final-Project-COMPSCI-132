ee6b0da3b5475c84f7b42045b341214e8dc6fcb2
Crackedform <acastform@gmail.com>
2026-04-28 16:40:48 -0400

    final update

--- Final_Project/Main.py
+++ Final_Project/Main.py
@@ -1,6 +1,6 @@
 import random
 def get_guess(max_num):
-    guess = input(f"Please input a number between 0 and {max_num} (No Spaces, decimals, or commas): ")
+    guess = input(f"Please input a number between 1 and {max_num} (No Spaces, decimals, or commas): ")
     if guess != "":
         for char in guess:
             if not (48 <= ord(char) <= 57):
@@ -17,12 +17,15 @@ def main():
     if mode == "E":
         max_attempts = 9
         max_num = 100
+        print(f"You will have {max_attempts} to guess the number between 1 and {max_num}")
     elif mode == "M":
         max_attempts = 8
         max_num = 200
+        print(f"You will have {max_attempts} to guess the number between 1 and {max_num}")
     elif mode == "H":
         max_attempts = 7
         max_num = 500
+        print(f"You will have {max_attempts} to guess the number between 1 and {max_num}")
     else:
         return -1
     attempts = 0
@@ -34,18 +37,20 @@ def main():
             print("Already guessed!")
         elif (guess == -1):
             print("Not a valid guess")
+        elif (guess > max_num or guess < 1):
+            print("Very funny, please input a number inside the given range")
         else:
             if(guess > number):
-                print("Too High")
+                print(f"Too High, {max_attempts - attempts - 1} left")
                 attempts += 1
                 guessed.append(guess)
             elif(guess < number):
-                print("Too Low")
+                print(f"Too Low, {max_attempts - attempts - 1} left")
                 guessed.append(guess)
                 attempts += 1
     if guess != number:
         print(f"Ran out of guesses! The number was {number}")
     else:
-        print(f"Congratulations! You guessed {number} correctly in {attempts} attempts")
+        print(f"Congratulations! You guessed {number} correctly in {attempts + 1} attempts")
 if __name__ == "__main__":
     main()
